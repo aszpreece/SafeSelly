7689018ae5618bc466b478db5dd7760870c79b35
jxc1089 <JXC1089@student.bham.ac.uk>
2018-11-04 00:14:41 +0000

    Added bottom nav

--- SafeSelly/app/src/main/java/com/safeselly/safeselly/MainActivity.java
+++ SafeSelly/app/src/main/java/com/safeselly/safeselly/MainActivity.java
@@ -1,32 +1,72 @@
 package com.safeselly.safeselly;
 
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActionBar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a fake list of earthquakes.
-        ArrayList<Crime> earthquakes = QueryUtils.extractCrimes();
+        toolbar = getSupportActionBar();
 
-        // Find a reference to the {@link ListView} in the layout
-        final ListView crimeListView = (ListView) findViewById(R.id.list);
+        setBottomNavigation();
 
-        // Create a new {@link EarthquakeAdapter} that takes a list of earthquakes as input
-        final CrimeAdapter adapter = new CrimeAdapter(this, earthquakes);
-
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        crimeListView.setAdapter(adapter);
+//        // Create a fake list of earthquakes.
+//        ArrayList<Crime> earthquakes = QueryUtils.extractCrimes();
+//
+//        // Find a reference to the {@link ListView} in the layout
+//        final ListView crimeListView = (ListView) findViewById(R.id.list);
+//
+//        // Create a new {@link EarthquakeAdapter} that takes a list of earthquakes as input
+//        final CrimeAdapter adapter = new CrimeAdapter(this, earthquakes);
+//
+//        // Set the adapter on the {@link ListView}
+//        // so the list can be populated in the user interface
+//        crimeListView.setAdapter(adapter);
     }
+
+    private void setBottomNavigation() {
+        BottomNavigationView bottomNav = findViewById(R.id.navigation_view);
+        // bottomNav.setSelectedItemId(R.id.navigation_map);
+        bottomNav.getMenu().findItem(R.id.navigation_map).setChecked(true);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.navigation_map:
+                        (Toast.makeText(getApplicationContext(), "Map", Toast.LENGTH_LONG)).show();
+                        break;
+                    case R.id.navigation_crimes:
+                        (Toast.makeText(getApplicationContext(), "Crimes", Toast.LENGTH_LONG)).show();
+                        break;
+                    case R.id.navigation_help:
+                        (Toast.makeText(getApplicationContext(), "Help", Toast.LENGTH_LONG)).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+
 }
